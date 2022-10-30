d6b34d523461c7ee814722ea65a224cb44240f17
pcbennett108 <pcbennett42@gmail.com>
2022-10-30 12:26:44 -0600

    Add request url for seperate pages

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  res.end("home page");
+  console.log(req.url);
+  const url = req.url;
+
+  //home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Home Page</h1>");
+    res.end();
+
+    //about page
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About Page</h1>");
+    res.end();
+  } else {
+    //404 page
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Not Found</h1>");
+    res.end();
+  }
 });
 
 server.listen(5000);
