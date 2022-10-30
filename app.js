bcac2a041092a47bcc41d130ef931a261d885155
pcbennett108 <pcbennett42@gmail.com>
2022-10-30 12:49:40 -0600

    manually add resources for styled page

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+//get all files
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
@@ -7,13 +14,25 @@ const server = http.createServer((req, res) => {
   //home page
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Home Page</h1>");
+    res.write(homePage);
     res.end();
 
-    //about page
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>About Page</h1>");
+    //styles
+  } else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+
+    //image logo
+  } else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
+    res.end();
+
+    //logic
+  } else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
     res.end();
   } else {
     //404 page
