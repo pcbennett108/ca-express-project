daa87535a926b7fecc87ef200eaef8669912183a
pcbennett108 <pcbennett42@gmail.com>
2022-11-01 13:06:33 -0600

    set up Public folder and serve with Express

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
+//setup static and middleware
+app.use(express.static("./public"));
+
 app.get("/", (req, res) => {
-  res.send("Home Page");
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Resource not Found");
 });
 
 app.listen(5000, () => {
   console.log("Server is listening on port 5000...");
 });
-
-//app.get
-//app.post
-//app.put
-//app.delete
-//app.all
-//app.use
-//app.listen
