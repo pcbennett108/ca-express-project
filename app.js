bc992f92d08a7bbf3cc309359cbb19e6da489bdd
pcbennett108 <pcbennett42@gmail.com>
2022-11-01 12:39:12 -0600

    reconfigure to use Express

--- app.js
+++ app.js
@@ -1,45 +1,18 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require("express");
+const app = express();
 
-//get all files
-const homePage = readFileSync("./navbar-app/index.html");
-const homeStyles = readFileSync("./navbar-app/styles.css");
-const homeImage = readFileSync("./navbar-app/logo.svg");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
-
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  const url = req.url;
-
-  //home page
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-
-    //styles
-  } else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(homeStyles);
-    res.end();
-
-    //image logo
-  } else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(homeImage);
-    res.end();
-
-    //logic
-  } else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homeLogic);
-    res.end();
-  } else {
-    //404 page
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>Not Found</h1>");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  res.send("Home Page");
 });
 
-server.listen(5000);
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000...");
+});
+
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
