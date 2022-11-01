4bc3d432c4d7f1aa992c58c5dfff904b37f5fae2
pcbennett108 <pcbennett42@gmail.com>
2022-11-01 14:33:05 -0600

    set server to deliver JSON data

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const express = require("express");
-const path = require("path");
+
 const app = express();
 
-//setup static and middleware
-app.use(express.static("./public"));
+const { products } = require("./data");
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-});
-
-app.all("*", (req, res) => {
-  res.status(404).send("Resource not Found");
+  res.json(products);
 });
 
 app.listen(5000, () => {
