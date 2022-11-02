2fba305b8590992c250d79ccd245c0a5bcef4d9d
pcbennett108 <pcbennett42@gmail.com>
2022-11-02 14:48:50 -0600

    Add query params

--- app.js
+++ app.js
@@ -27,6 +27,31 @@ app.get("/api/products/:productID", (req, res) => {
   res.json(singleProduct);
 });
 
+app.get("api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("Hello World");
+});
+
+app.get("/api/v1/query", (req, res) => {
+  //console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    //res.status(200).send("No products found.");
+    return res.status(200).json({ success: true, data: [] });
+  }
+  res.status(200).json(sortedProducts);
+});
+
 app.listen(5000, () => {
   console.log("Server is listening on port 5000...");
 });
