3be9932cbb0e2d57a2d5c06c13f5b0a43f891062
pcbennett108 <pcbennett42@gmail.com>
2022-11-02 12:54:12 -0600

    Add route params

--- app.js
+++ app.js
@@ -5,7 +5,26 @@ const app = express();
 const { products } = require("./data");
 
 app.get("/", (req, res) => {
-  res.json(products);
+  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+});
+
+app.get("/api/products", (req, res) => {
+  const newProduct = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProduct);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product does not exist");
+  }
+  res.json(singleProduct);
 });
 
 app.listen(5000, () => {
